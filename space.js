ac978cfad9f8ac12d618eaf5bda8e320e5302b05
Svetlana Malikova <s.u.malikova@gmail.com>
2016-05-26 23:56:17 +0300

    Начало

---
+++ space.js
@@ -0,0 +1,17 @@
+"use strict";
+
+var earth = {};
+	earth.weight = 5.9726e24;
+	earth.radius = 6371000;
+	earth.X = 0;
+	earth.Y = 0;
+	earth.speedX = 0;
+	earth.speedY = 0;
+
+var moon = {};
+	moon.weight = 7.3477e10;
+	moon.radius = 1737100;
+	moon.X = 0;
+	moon.Y = 362600000;
+	moon.speedX = 1090;
+	moon.speedY = 0;
