f56ca9c6d565742a44a2ec7e43c1adc928faedca
Svetlana Malikova <s.u.malikova@gmail.com>
2016-05-27 23:20:38 +0300

    Создан canvas

--- space.js
+++ space.js
@@ -1,5 +1,9 @@
 "use strict";
 
+var modelTime = 0;
+
+var sysTime = 0;
+
 var earth = {
 	mass: 5.9726e24,
 	radius: 6371000,
@@ -17,5 +21,27 @@ var moon = {
 	speedX: 1090,
 	speedY: 0
 };
+
+var mainloop = function() {
+	//расчет();
+	draw();
+};
+
+setInterval(mainloop);
+
+function draw() {
+	var canvas  = document.getElementById("canvas");
+	canvas.width  = 600; 
+	canvas.height = 600; 
+		
+	var context = canvas.getContext('2d');
+	context.fillStyle   = '#000'; 
+	context.fillRect(0, 0, canvas.width, canvas.height);	
+		
+	context.beginPath();
+	//..........
+	context.stroke();	
+}
+
 
 
