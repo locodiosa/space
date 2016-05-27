7e6a784c0b15794b5b05c78b9f1da9f708b3deff
Svetlana Malikova <locodiosa@mail.ru>
2016-05-27 16:51:53 +0300

    update

--- space.js
+++ space.js
@@ -1,17 +1,20 @@
 "use strict";
 
-var earth = {};
-	earth.weight = 5.9726e24;
-	earth.radius = 6371000;
-	earth.X = 0;
-	earth.Y = 0;
-	earth.speedX = 0;
-	earth.speedY = 0;
-
-var moon = {};
-	moon.weight = 7.3477e10;
-	moon.radius = 1737100;
-	moon.X = 0;
-	moon.Y = 362600000;
-	moon.speedX = 1090;
-	moon.speedY = 0;
+var earth = {
+	weight = 5.9726e24;
+	radius = 6371000;
+	x = 0;
+	y = 0;
+	speedX = 0;
+	speedY = 0;
+};
+	
+var moon = {
+	weight = 7.3477e10;
+	radius = 1737100;
+	x = 0;
+	y = 362600000;
+	speedX = 1090;
+	speedY = 0;
+};
+	
