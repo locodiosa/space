e1976a3cbdef201c465c78bfd3e7826b6c72e93e
Svetlana Malikova <s.u.malikova@gmail.com>
2016-05-29 23:25:02 +0300

    Создан массив объектов - spaceObjects

--- space.js
+++ space.js
@@ -4,23 +4,27 @@ var modelTime = 0;
 
 var sysTime = 0;
 
-var earth = {
-	mass: 5.9726e24,
-	radius: 6371000,
-	x: 0,
-	y: 0,
-	speedX: 0,
-	speedY: 0
-};
+var spaceObjects = [
+	{
+		name: "Earth",
+		mass: 5.9726e24,
+		radius: 6371000,
+		x: 0,
+		y: 0,
+		speedX: 0,
+		speedY: 0
+	},
 	
-var moon = {
-	mass: 7.3477e10,
-	radius: 1737100,
-	x: 0,
-	y: 362600000,
-	speedX: 1090,
-	speedY: 0
-};
+	{
+		name: "Moon",
+		mass: 7.3477e10,
+		radius: 1737100,
+		x: 0,
+		y: 362600000,
+		speedX: 1090,
+		speedY: 0
+	}
+];
 
 var mainloop = function() {
 	//расчет();
